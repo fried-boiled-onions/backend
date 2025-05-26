0993f04c78dc5fbd76552fd46e476c8e7c5faea6
Gleb Vvedensky <stxreocoma@planshet.local>
2025-05-26 19:19:26 +0300

    WIP: added dockerfile and docker compose

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2632f36f5a5e304e743d5948d6694632c50bf55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d236ef80f30fea6db685013fde00cde4b65278e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
